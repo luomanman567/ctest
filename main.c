9a7de1484b3b3437982d32f4cf3e06a0de6fc2a0
luomanruo <luomanruo@espressif.com>
2023-06-15 17:23:21 +0800

    workflow test

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main()
 {
     int8_t flag = 1;
     uint8_t idx;
-    for(int i = 0; i < 100; i++)
+    for(int i = 0; i < 200; i++)
     {
         idx = flag & (-flag);
         printf("flag, idx: %d %d\n", flag, idx);
